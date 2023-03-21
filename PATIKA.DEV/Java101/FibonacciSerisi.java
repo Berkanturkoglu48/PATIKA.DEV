e2989f029dcb5914ed353d0d680ed05ff4627766
Berkan Türkoğlu <86087376+Berkanturkoglu48@users.noreply.github.com>
2023-03-21 16:42:56 +0300

    Fibonacci serisi bulan program geliştirildi.

---
+++ PATIKA.DEV/Java101/FibonacciSerisi.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class FibonacciSerisi {
+    public static void main(String[] args) {
+        int limit;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Fibonacci serinizin eleman sayısı kaç olsun ? :");
+        limit= input.nextInt();
+
+        int a = 0, b=1;
+
+        System.out.print(limit + " Elemanlı Fibonacci Serisi : ");
+        System.out.print("0 "+ "1 ");
+        int i =1, c;
+        while(i<=limit-2){
+            c=a+b;
+            a=b;
+            b=c;
+            System.out.print(c +" ");
+            i++;
+
+
+        }
+
+
+
+
+
+    }
+}
